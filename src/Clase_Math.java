2968e93e69d90eeb26e5729a44f47639e3712d3e
Francisco Garcia <tecatito585@gmail.com>
2023-04-01 19:32:39 -0700

    New files

---
+++ src/Clase_Math.java
@@ -0,0 +1,28 @@
+
+public class Clase_Math {
+	
+	public static void main(String[] args) {
+		//Obtener Raiz Cuadrada
+		/*int raiz = (int)Math.sqrt(9);
+		double raiz1 = Math.sqrt(10);
+		System.out.println(raiz);
+		System.out.println(raiz1);*/
+		
+		//Elevacion de Potencia
+		/*
+		double base = 5, exponente = 2;
+		double resultado = Math.pow(base, exponente);
+		System.out.println(resultado);*/
+		
+		//Redondear un numero
+		/*double numero = 4.56;
+		long resultado =Math.round(numero);
+		
+		System.out.println(resultado);*/
+		
+		double numero = Math.random();
+		System.out.println(numero);
+		
+		
+	}
+}
